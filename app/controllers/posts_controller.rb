254152d2e9431d2210061e5e12eb7cbec3a90368
mzw86020424 <mzw86020424@gmail.com>
2020-12-09 17:56:01 +0900

    URLを年・月と合わせて保存する機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,18 @@
 class PostsController < ApplicationController
   def index
+    @post = Post.new
+  end
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render root_path
+    end
+  end
+
+  def post_params
+    t = Time.new
+    params.require(:post).permit(:url).merge(user_id: current_user.id, month:t.month, year:t.year)
   end
 end

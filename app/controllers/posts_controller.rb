130cb3d9b764c55fa4e652182c6f444c5ddf2518
mzw86020424 <mzw86020424@gmail.com>
2020-12-10 11:36:19 +0900

    thumbalizrを導入

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,7 @@ class PostsController < ApplicationController
     @post = Post.new
     @posts = Post.where(user_id: current_user.id).where(month: t.month).where(year: t.year)
   end
+
   def create
     @post = Post.new(post_params)
     if @post.save
@@ -13,6 +14,7 @@ class PostsController < ApplicationController
     end
   end
 
+  
   def post_params
     t = Time.new
     params.require(:post).permit(:url).merge(user_id: current_user.id, month:t.month, year:t.year)

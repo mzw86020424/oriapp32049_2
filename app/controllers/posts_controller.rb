bb77834116d2426412d79b2ad779bd73ecb97cc4
mzw86020424 <mzw86020424@gmail.com>
2020-12-14 18:24:54 +0900

    ヘッダーに年月を表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   def index
     t = Time.new
     if Zine.where(month: t.month).where(year: t.year).where(user_id: current_user.id).exists?
-      @zine = Zine.where(month: t.month).where(year: t.year).where(user_id: current_user.id)
+      @zine = Zine.find_by(month: t.month, year: t.year, user_id: current_user.id)
     else
       @zine = Zine.new
       @zine = Zine.create(zine_params)
--- app/controllers/zines_controller.rb
+++ app/controllers/zines_controller.rb
@@ -4,6 +4,6 @@ class ZinesController < ApplicationController
   end
   def show
     @zine = Zine.find(params[:id])
-    @posts = Post.where(user_id: current_user.id).where(month: @zine.month).where(year: @zine.year).order("created_at DESC")
+    @posts = Post.where(user_id: current_user.id).where(month: @zine.month).where(year: @zine.year)
   end
 end
